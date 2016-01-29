f98884925272f8cb41edd0c3b6cf46eea239c227
JASPER LANCASTER <jasperlancaster@JASPERs-MacBook-Pro.local>
2016-01-29 16:57:49 +0900

    added files

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str = str + ", you're a fagwar"
+var numA = 1.5
+var numB = numA * 15
+var numC = numB / 2
+var numD = numC - 5
+var remainder = 45 % 10
+
+
+
+
+
